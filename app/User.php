50008c10d82b4be84b3e9952cef539dfbf14ead8
Willy Everton S. Nascimento <willy.everton.s.nascimento@gmail.com>
2020-07-27 07:27:22 -0300

    Upload to Job in Directorie

--- app/User.php
+++ app/User.php
@@ -36,4 +36,19 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function adminlte_image()
+    {
+        return "img/logo-min.jpeg";
+    }
+
+    public function adminlte_desc()
+    {
+        return "Description Test";
+    }
+
+    public function adminlte_profile_url()
+    {
+        return "user/1";
+    }
 }
